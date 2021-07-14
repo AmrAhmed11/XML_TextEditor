390462fc58529b471840bf12c8cd6296894450a5
Omar Rashed <70315665+OmarRash@users.noreply.github.com>
2021-07-14 14:31:21 +0200

    bug fix

--- XMLparser/string_manipulate.cpp
+++ XMLparser/string_manipulate.cpp
@@ -1,5 +1,4 @@
 #include "string_manipulate.h"
-#include<algorithm>
 using namespace std;
 
 string trim_extra_mid_spaces(string s) {
@@ -44,7 +43,7 @@ std::string trim(const std::string& s) {
 }
 
 
-void stringManipulate(string input, vector<string>& tag, vector<string>& tagend, vector<string>& data, vector<string>& attr) {
+void stringManipulate(string input) {
     int index = 0;
     int len = input.length();
     int line = 0;
@@ -58,34 +57,28 @@ void stringManipulate(string input, vector<string>& tag, vector<string>& tagend,
                         tagstr = input.substr(index + 1, i);
                         tagstr = trim(tagstr);
                         opening(tagstr, index + 1);
-
-                        tag.push_back(tagstr);
                         index += i + 1;
                         break;
                     }
                 }
+                //attributes and self closing tag
                 if (input[index] == ' ') {
                     for (int i = index; i < index + 300; i++) {
                         if (input[i] == '>') {
-                            if (input[i - 1] == '/')
+                            int selfclosing= i - 1;
+                            i = i - index - 1;
+                            attrstr = input.substr(index + 1, i);
+                            attrstr = trim_extra_mid_spaces(attrstr);
+                            attrstr = trim(attrstr);
+                            // check if self closing
+                            if (input[selfclosing] == '/')
                             {
-                                i = i - index - 1;
-                                attrstr = input.substr(index + 1, i);
-                                //function to set attr with sub value
-                                attrstr = trim(attrstr);
                                 setAttribute(attrstr, 1);
-                                attr.push_back(attrstr);
+                                closing (tagstr);
                                 index += i + 1;
                                 break;
                             }
-                            i = i - index - 1;
-                            attrstr = input.substr(index + 1, i);
-                            //function to set attr with sub value
-
-                            attrstr = trim_extra_mid_spaces(attrstr);
-                            attrstr = trim(attrstr);
                             setAttribute(attrstr, 0);
-                            attr.push_back(attrstr);
                             index += i + 1;
                             break;
                         }
@@ -101,14 +94,12 @@ void stringManipulate(string input, vector<string>& tag, vector<string>& tagend,
                         tagendstr = trim(tagendstr);
                         //function to finish node with sub value
                         closing(tagendstr);
-
-                        tagend.push_back(tagendstr);
                         index += (i + 2);
                         break;
                     }
                 }
             }
-            //comments handling
+            //comments tag
             else if (input[index + 1] == '!') {
                 opening("", index + 1);
                 for (int i = index; i < index + 2200; i++) {
@@ -119,22 +110,19 @@ void stringManipulate(string input, vector<string>& tag, vector<string>& tagend,
                         datastr = trim(datastr);
                         setAttribute(datastr, 2);
                         closing("");
-                        data.push_back(datastr);
                         index += i;
                         break;
                     }
                 }
             }
+            //XML tag 
             else if (input[index + 1] == '?') {
-                opening("", index + 1);
                 for (int i = index; i < index + 20; i++) {
                     if (isspace(input[i]) || input[i] == '>') {
                         i = i - index - 1;
                         tagstr = input.substr(index + 1, i);
                         tagstr = trim(tagstr);
                         opening(tagstr, index + 1);
-
-                        tag.push_back(tagstr);
                         index += i + 1;
                         break;
                     }
@@ -145,11 +133,9 @@ void stringManipulate(string input, vector<string>& tag, vector<string>& tagend,
                             i = i - index - 1;
                             attrstr = input.substr(index + 1, i);
                             //function to set attr with sub value
-
                             attrstr = trim_extra_mid_spaces(attrstr);
                             attrstr = trim(attrstr);
                             setAttribute(attrstr, 4);
-                            attr.push_back(attrstr);
                             closing(tagstr);
                             index += i + 1;
                             break;
@@ -166,8 +152,6 @@ void stringManipulate(string input, vector<string>& tag, vector<string>& tagend,
                     datastr = input.substr(index, i + 1);
                     datastr = trim(datastr);
                     setData(datastr);
-
-                    data.push_back(datastr);
                     index += i;
                     break;
                 }
--- XMLparser/string_manipulate.h
+++ XMLparser/string_manipulate.h
@@ -1,12 +1,13 @@
 #ifndef STRING_MANIPULATE_CPP
 #define STRING_MANIPULATE_CPP
 
+#include<algorithm>
 #include "types.h"
 #include "opening.h"
 #include "closing.h"
 
 using namespace std;
 
-void stringManipulate(string input, vector<string>& tag, vector<string>& tagend, vector<string>& data, vector<string>& attr);
+void stringManipulate(string input);
 
 #endif
